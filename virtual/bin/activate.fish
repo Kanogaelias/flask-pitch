f9a28bff39397f88e2f9c36258620fbed3f27aed
Kanogaelias <kanogae@gmail.com>
2018-11-18 08:26:20 +0300

    delete app file

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/elias/blog/virtual"
+set -gx VIRTUAL_ENV "/home/elias/Documents/blog/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
