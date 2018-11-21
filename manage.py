9df6d3bfdef371316b6f7a36a805c79f28b99653
Kanogaelias <kanogae@gmail.com>
2018-11-21 10:52:08 +0300

    set to production

--- manage.py
+++ manage.py
@@ -5,7 +5,8 @@ from app.models import User,Role,Blog,Comment,Category,Subscribe
 from  flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
-app = create_app('development')
+# app = create_app('development')
+app = create_app('production')
 
 
 migrate = Migrate(app,db)
